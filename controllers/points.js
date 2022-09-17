fef5620285ffa7d55618f914c963680f7dde10b9
Bre <breanna.schmidt12@gmail.com>
2022-09-17 14:08:57 -0400

    added points controller. Testing out different ways to do this

---
+++ controllers/points.js
@@ -0,0 +1,36 @@
+const {Payer, Points} = require('../models');
+
+const pointsController = {
+	addPoints({params, body}, res) {
+		Points.create(body)
+		.then(({_id}) => {
+			return Payer.findOneAndUpdate(
+				{ _id: params.payerId },
+				{ $push:{points: _id} },
+				{new: true }
+			);
+		})
+		.then((dbNewPoints) => {
+			if(!dbNewPoints) {
+				res.json({message:'Cannot add points to this id. Id is not found'})
+			}
+			res.status(200).json(dbNewPoints)
+		})
+		.catch((err) => { res.json(err) });
+	},
+
+	spendPoints({params}, res) {
+		Points.findOneAndDelete({_id: params.id})
+		.then((dbSpendPts) => {
+			if(dbSpendPts > Points.total) {
+				res.json({message:'Cannot spend points. Total cannot go below 0'})
+			} else if (!dbSpendPts) {
+				res.json({message:'Cannot find points with that id'})
+			}
+			res.status(200).json(dbSpendPts)
+		})
+		.catch((err) => { res.status(400).json(err) })
+	}
+};
+
+module.exports = pointsController;
