ae08b71f4d1a6d185e16fb4918044df4ce98813b
Bre <breanna.schmidt12@gmail.com>
2022-09-17 20:48:48 -0400

    fixed bug, had path to routes wrong

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const payerRoutes = require('../../controllers/payers');
-const pointsRoutes = require('../../controllers/points');
+const payerRoutes = require('./payer-routes');
+const pointsRoutes = require('./points-routes');
 
 router.use('/payers', payerRoutes);
 router.use('/points', pointsRoutes)
